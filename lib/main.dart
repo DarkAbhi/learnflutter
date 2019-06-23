ba83e261da67cb6e6cea538e510f49678492fadb
Abhishek AN <darkabhi1520@gmail.com>
2019-06-23 12:32:28 +0530

    Use list and learn to use dynamically adding new card
    Signed-off-by: Abhishek AN <darkabhi1520@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _pictures = ['Scenic Lake'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
-            title: Text('Learner App'),
+            title: Text('Pictures Showcase'),
           ),
           body: Column(
             children: [
               Container(
                 margin: EdgeInsets.all(10.0),
                 child: RaisedButton(
-                  onPressed: () {},
-                  child: Text('View'),
+                  onPressed: () {
+                    setState(() {
+                      _pictures.add('Abstract');
+                    });                    
+                  },
+                  child: Text('Add Image'),
                 ),
               ),
-              Card(
-                child: Column(
-                  children: <Widget>[
-                    Image.asset('assets/scene.jpg'),
-                    Text('Scenery')
-                  ],
-                ),
+              Column(
+                children: _pictures
+                    .map((element) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/scene.jpg'),
+                              Text(element)
+                            ],
+                          ),
+                        ))
+                    .toList(),
               ),
             ],
           )),
